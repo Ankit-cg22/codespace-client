fabbf8d77626ec22c5bd6524580198d3e8c4f6d1
Ankit-cg22 <ankitdas.cg22@gmail.com>
2022-10-17 01:54:11 +0530

    minor changes

--- utils/socket.js
+++ utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 import { BACKEND_URL } from './constants';
-export const socket = io(BACKEND_URL)
+export const socket = io(BACKEND_URL ,{ transports: ['websocket', 'polling', 'flashsocket'] })
 
 
